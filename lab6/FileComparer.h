ba00b68d9f2857d3ce3d778e6fe8665f24129539
4z0t <gastec139@gmail.com>
2021-12-05 12:59:25 +0300

    Добавьте файлы проекта.

---
+++ lab6/FileComparer.h
@@ -0,0 +1,200 @@
+#pragma once
+#include<iostream>
+#include <fstream>
+#include <mutex>
+#include<thread>
+#include <future>
+#include <chrono>
+#include <memory>
+#include <filesystem>
+#include<algorithm>
+
+
+
+
+class FileComparer
+{
+	typedef std::filesystem::path fspath;
+	typedef std::filesystem::directory_entry fs_directory_entry;
+	typedef std::shared_ptr<fs_directory_entry> fs_directory_entry_ptr;
+	typedef std::pair< fs_directory_entry_ptr, fs_directory_entry_ptr> fs_directory_entry_ptr_pair;
+
+	std::vector<
+		std::unique_ptr<
+		std::pair<
+		std::shared_ptr<
+		fs_directory_entry_ptr_pair
+		>,
+		std::future<bool>//result of comparison
+		>
+		>
+	> results;
+	std::vector<fs_directory_entry_ptr> files;
+	std::mutex mut;
+	bool is_done;
+public:
+	std::mutex output;
+	FileComparer() :files(), results(), is_done(false)
+	{
+
+
+	}
+	void start(const fspath& path)
+	{
+		std::thread go_through_dirs_thread(&FileComparer::go_through_dirs, this, std::ref(path));
+		std::thread distributor_thread(&FileComparer::distributor, this);
+		std::thread dumper_thread(&FileComparer::dumper, this);
+		go_through_dirs_thread.join();
+		distributor_thread.join();
+		dumper_thread.join();
+
+		std::cout << "Files scaned: " << this->files.size() << std::endl;
+		/*for (auto& p : this->results)
+		{
+			if (p->second.get())
+			{
+				std::cout << "Found equal files:\n" << '\t' << p->first->first->path().relative_path() << "\n\t" << p->first->second->path().relative_path() << std::endl;
+			}
+		}*/
+
+	}
+private:
+	inline static bool shallow_compare(const fs_directory_entry& de1, const fs_directory_entry& de2)
+	{
+		return de1.file_size() == de2.file_size();	
+	}
+	//TODO:
+	// use hashing  instead
+	static bool deep_compare(const fs_directory_entry& de1, const fs_directory_entry& de2)
+	{
+		std::ifstream f1(de1.path(), std::ios::binary);
+		std::ifstream f2(de2.path(), std::ios::binary);
+		if (f1.is_open() && f2.is_open())
+		{
+			/*f1.open(de1.path());
+			f2.open(de2.path());*/
+			while (!f1.eof() && !f2.eof())
+				if (f1.get() != f2.get())
+				{
+
+					f1.close();
+					f2.close();
+					return false;
+				}
+
+			bool res = (f1.eof() == f2.eof());
+			f1.close();
+			f2.close();
+			return res;
+
+		}
+		return false;
+
+	}
+
+
+	void dumper()
+	{
+		std::ofstream file("result2.txt", std::ios::ate);
+		if (!file.is_open()) throw std::exception("cant open file for dump!");
+		while (true)
+		{
+			std::lock_guard lock(this->mut);
+			while (true) {
+				if (this->results.size()) {
+					auto res = std::move(*this->results.back());
+					std::async(std::launch::async, [&res, this, &file] {
+						auto resl = std::move(res);
+						if (resl.second.get())
+						{
+							std::lock_guard lock(this->output);
+							file << "Found equal files:\n" << '\t' << resl.first->first->path() << "\n\t" << resl.first->second->path() << std::endl;
+						}
+						});
+					this->results.pop_back();
+				}
+				else {
+					if (this->is_done)return;
+					break;
+				}
+			}
+		}
+		file.close();
+	}
+
+	void go_through_dirs(const fspath& path)
+	{
+
+		for (auto& p : std::filesystem::recursive_directory_iterator(path))
+		{
+			//std::cout << "found " << p.path() << std::endl;
+			std::lock_guard lock(this->mut);
+			if (p.is_regular_file())
+				this->files.push_back(std::make_shared<fs_directory_entry>(p));
+		}
+		this->is_done = true;
+	}
+
+	//distributes files to be compared
+	void distributor()
+	{
+		//std::vector <std::shared_ptr<fs_directory_entry_ptr_pair>> file_pairs;
+		int last_end = this->files.size();
+
+		while (!this->is_done)
+		{
+
+			std::lock_guard lock(this->mut);
+			if (last_end != this->files.size()) {
+				for (auto path_p1 = this->files.begin(); path_p1 != this->files.end(); path_p1++)
+				{
+
+					for (auto path_p2 = (this->files.begin() + last_end > path_p1) ? last_end + this->files.begin() : path_p1 + 1; path_p2 != this->files.end(); path_p2++)
+					{
+						//if (!this->is_in_pairs(file_pairs, *path_p1, *path_p2))
+						if(shallow_compare(**path_p1, **path_p2))
+						{
+							fs_directory_entry_ptr_pair files_pair;
+
+
+							files_pair.first = *path_p1;
+							files_pair.second = *path_p2;
+							std::pair<
+								std::shared_ptr<
+								fs_directory_entry_ptr_pair
+								>,
+								std::future<bool>//result of comparison
+							> res;
+							res.first = std::make_shared<fs_directory_entry_ptr_pair>(files_pair);
+							res.second = std::async(std::launch::async, &FileComparer::deep_compare, std::ref(**path_p1), std::ref(**path_p2));
+							this->results.push_back(std::make_unique<std::pair<
+								std::shared_ptr <
+								fs_directory_entry_ptr_pair>,
+								std::future<bool>//result of comparison
+								>>());
+							*this->results.back() = std::move(res);
+							//file_pairs.push_back(this->results.back()->first);
+						}
+					}
+				}
+			}
+			last_end = this->files.size();
+		}
+
+
+	}
+
+	bool is_in_pairs(const std::vector <std::shared_ptr<fs_directory_entry_ptr_pair>>& file_pairs,
+		const fs_directory_entry_ptr& p1,
+		const fs_directory_entry_ptr& p2)
+	{
+		for (auto& pair : file_pairs)
+		{
+			if ((pair->first == p1 and pair->second == p2) or (pair->first == p2 and pair->second == p1))
+				return true;
+		}
+		return false;
+	}
+
+};
+
---
+++ lab6/lab6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <filesystem>
+#include "FileComparer.h"
+
+
+
+int main()
+{
+
+	//int n = 8;
+	//cout << n % 11 + 1 << endl;//9
+	//cout << ((n * n * n) % 11 + 3) % 11 + 1 << endl;//10
+	
+	FileComparer fc;
+	//fc.start("C:\\Users\\4z0t\\Documents\\lab6test");
+	fc.start("E:\\Program Files (x86)");
+	/*try {
+
+	}
+	catch (std::exception ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}*/
+	//std::cin.get();
+
+
+
+}
